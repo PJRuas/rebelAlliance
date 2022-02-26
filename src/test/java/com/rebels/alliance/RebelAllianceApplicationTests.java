d7cb79a79bbbdc4c4689c393bbbac6eb5a6cf5f1
Pedro Ruas <pjruas123@gmail.com>
2022-02-26 17:07:03 -0300

    Creation
    created repository

---
+++ src/test/java/com/rebels/alliance/RebelAllianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebels.alliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebelAllianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
